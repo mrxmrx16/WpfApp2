2e7fd864144b67d1519a008fea94e5616621fd05
mrxmrx16 <matbag8085@gmail.com>
2024-12-19 15:08:39 +0400

    Add project files.

---
+++ WpfApp2/ViewModel/ButtonViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp2.Service;
+
+namespace WpfApp2.ViewModel
+{
+    internal class ButtonViewModel
+    {
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                    (addCommand = new RelayCommand(obj =>
+                    {
+                        MessageBox.Show("");
+                    }));
+            }
+        }
+    }
+}
---
+++ WpfApp2/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2.ViewModel
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private List<Button> listButton;
+        public List<Button> ListButton
+        {
+            get { return listButton; }
+            set 
+            { 
+                listButton = value;
+                OnPropertyChanged("ListButton");
+            }
+        }
+
+        public MainViewModel()
+        {
+            ListButton = new List<Button>();
+
+            for (int i=0; i<10; i++)
+            {
+                Button button = new Button();
+                button.Name = "";
+                ButtonViewModel buttonView = new ButtonViewModel();
+                button.DataContext = buttonView;
+                button.Command = buttonView.AddCommand;
+                button.Margin = new Thickness(20, 20, 20, 20);
+                ListButton.Add(button);
+            }
+
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
